2e1cf73e69e180fbdf72311f2cc8f9f911ccf1aa
Sean Donahoe <sean@seandonahoe.com>
2026-05-15 09:53:41 +0700

    test: add ipcBridge.acpConversation.getLoadErrors to mock (L20 followup)
    L20 (78c6b90c9) added getLoadErrors to ipcBridge.acpConversation and
    acpConversationBridge.ts registers its provider. The test's hoisted
    mock of '../../src/common' didn't include the new channel, so all 6
    acpConversationBridge tests failed with 'provider is undefined'.
    
    Append getLoadErrors: makeChannel('getLoadErrors') to the mock. 6/6
    passing.

--- tests/unit/acpConversationBridge.test.ts
+++ tests/unit/acpConversationBridge.test.ts
@@ -19,6 +19,7 @@ vi.mock('../../src/common', () => ({
       checkEnv: makeChannel('checkEnv'),
       detectCliPath: makeChannel('detectCliPath'),
       getAvailableAgents: makeChannel('getAvailableAgents'),
+      getLoadErrors: makeChannel('getLoadErrors'),
       refreshCustomAgents: makeChannel('refreshCustomAgents'),
       testCustomAgent: makeChannel('testCustomAgent'),
       checkAgentHealth: makeChannel('checkAgentHealth'),
